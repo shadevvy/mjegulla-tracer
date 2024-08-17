d9c6243a94cacde3f5e507b6eb5a017b1e8d7754
Shalaka Deshan <162401543+shadevvy@users.noreply.github.com>
2024-08-17 14:39:25 +0400

    Add Traffic model and migration and modified Sites model

--- app/Models/Traffic.php
+++ app/Models/Traffic.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Site extends Model
+class Traffic extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'url',
+        'site_id',
+        'user_ip',
+        'country',
+        'device',
+        'user_agent',
     ];
 }
